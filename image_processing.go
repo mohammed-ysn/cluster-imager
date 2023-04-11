5b31ea55b2bac4c726d4e2ba152cdbc77e8816f7
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-04-11 18:35:32 +0100

    Cap cropped image to input image

--- image_processing.go
+++ image_processing.go
@@ -10,7 +10,8 @@ import (
 // The resulting image will have the specified width and height,
 // and will be positioned at (x, y) within the input image.
 func cropImage(inputImg image.Image, x, y, width, height int) image.Image {
-	cropRect := image.Rect(0, 0, width, height)
+	cropRect := inputImg.Bounds().Intersect(image.Rect(x, y, x+width, y+height))
+
 	croppedImage := image.NewRGBA(cropRect)
 
 	draw.Draw(croppedImage, cropRect, inputImg, image.Pt(x, y), draw.Src)
--- image_processing_test.go
+++ image_processing_test.go
@@ -7,11 +7,22 @@ import (
 	"testing"
 )
 
-func TestCropImage(t *testing.T) {
+func TestCropImageDimensionsWithZeroPosition(t *testing.T) {
+	testImg := image.NewRGBA(image.Rect(0, 0, 100, 100))
+
+	croppedImg := cropImage(testImg, 0, 0, 50, 50)
+
+	// check cropped image dimensions
+	if croppedImg.Bounds().Dx() != 50 || croppedImg.Bounds().Dy() != 50 {
+		t.Errorf("Expected dimensions: 50x50. Actual dimensions: %dx%d", croppedImg.Bounds().Dx(), croppedImg.Bounds().Dy())
+	}
+}
+
+func TestCropImageWithNonZeroPosition(t *testing.T) {
 	// set up dimensions for the test image
 	// and the desired cropped image
 	beforeX, beforeY := 400, 400
-	afterX, afterY := 100, 100
+	afterX, afterY := 10, 10
 	cropStartX, cropStartY := 250, 250
 
 	// create the test image
@@ -21,18 +32,37 @@ func TestCropImage(t *testing.T) {
 
 	croppedImg := cropImage(testImg, cropStartX, cropStartY, afterX, afterY)
 
-	// check cropped image dimensions
 	if croppedImg.Bounds().Dx() != afterX || croppedImg.Bounds().Dy() != afterY {
 		t.Errorf("Cropped image has unexpected dimensions: %dx%d", croppedImg.Bounds().Dx(), croppedImg.Bounds().Dy())
 	}
 
 	// check all pixel colours in cropped image
-	black_rgba := color.RGBAModel.Convert(color.Black)
 	for x := 0; x < afterX; x++ {
 		for y := 0; y < afterY; y++ {
-			if croppedImg.At(x, y) != black_rgba {
-				t.Errorf("Pixel at (%d, %d) is %v instead of %v", x, y, croppedImg.At(x, y), color.Black)
+			r, g, b, _ := croppedImg.At(x, y).RGBA()
+			if r != 0 || g != 0 || b != 0 {
+				t.Errorf("Pixel at (%d, %d) is (%v,%v,%v) instead of (0,0,0)", x, y, r, g, b)
 			}
 		}
+	}
+}
+
+func TestCropImageWithWidthGreaterThanImageWidth(t *testing.T) {
+	testImg := image.NewRGBA(image.Rect(0, 0, 100, 100))
+
+	croppedImg := cropImage(testImg, 0, 0, 150, 50)
+
+	if croppedImg.Bounds().Dx() != 100 || croppedImg.Bounds().Dy() != 50 {
+		t.Errorf("Expected dimensions: 100x50. Actual dimensions: %dx%d", croppedImg.Bounds().Dx(), croppedImg.Bounds().Dy())
+	}
+}
+
+func TestCropImageWithHeightGreaterThanImageHeight(t *testing.T) {
+	testImg := image.NewRGBA(image.Rect(0, 0, 100, 100))
+
+	croppedImg := cropImage(testImg, 0, 0, 50, 150)
+
+	if croppedImg.Bounds().Dx() != 50 || croppedImg.Bounds().Dy() != 100 {
+		t.Errorf("Expected dimensions: 50x100. Actual dimensions: %dx%d", croppedImg.Bounds().Dx(), croppedImg.Bounds().Dy())
 	}
 }
