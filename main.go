6e0615182f02edd21cf21406e1524f913f9cfcc0
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-05-14 02:36:18 +0100

    Add image processing to server

--- main.go
+++ main.go
@@ -1,16 +1,74 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/mohammed-ysn/cluster-imager/image_processing"
+	"image"
+	"image/jpeg"
+	"log"
 	"net/http"
+	"strconv"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("New connection")
-		fmt.Fprintf(w, "If you see this response... success!")
-	})
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
+	fmt.Println("Handling new request")
+
+	// parse the multipart form data
+	err := r.ParseMultipartForm(10 << 20) // 10 MB max file size
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// get the uploaded file
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// decode the uploaded image
+	inputImg, _, err := image.Decode(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// call image processing functions
+	// TODO: remove hard-coded values
+	processedImage := image_processing.CropImage(inputImg, 100, 50, 60, 60)
+
+	// encode the processed image as JPEG
+	var buf bytes.Buffer
+	err = jpeg.Encode(&buf, processedImage, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// set the response headers
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buf.Bytes())))
+
+	// write the processed image to the response writer
+	_, err = w.Write(buf.Bytes())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/image", imageHandler)
 	fmt.Println("Server started on port 8080")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
